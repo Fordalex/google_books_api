3262ae3131b6c287899527d8c7b6837d3933d2ca
Alex <alex96ford19@gmail.com>
2021-04-02 16:38:17 +0100

    Added the script to host the project on heroku.

--- server.js
+++ server.js
@@ -15,7 +15,15 @@ app.use("/api/auth", require("./routes/api/auth"));
 app.use("/api/profile", require("./routes/api/profile"));
 app.use("/api/books", require("./routes/api/books"));
 
-app.get("/", (req, res) => res.send("API Running"));
+// Serve static assets in production
+if(process.env.NODE_ENV === 'production') {
+    // set static folder
+    app.use(express.static('client/build'));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+    });
+}
 
 const PORT = process.env.PORT || 5000;
 
